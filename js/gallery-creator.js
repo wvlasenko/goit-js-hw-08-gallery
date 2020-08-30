442d1c0a34e3a0d47cd258a3334621021737bbc9
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2020-08-30 19:29:36 +0300

    Add function close momal wimdow whrn press esc

--- js/gallery-creator.js
+++ js/gallery-creator.js
@@ -1,14 +1,19 @@
 import images from './gallery-items.js';
 
 const galleryContainer = document.querySelector('.js-gallery');
-// let indexOfImage = -1;
 const lightBox = document.querySelector('div.js-lightbox');
+// const lightBoxOverlay = document.querySelector('div.lightbox__overlay');
+const lightBoxOverlay = document.querySelector('div.lightbox__content');
 const lightBoxImage = document.querySelector('img.lightbox__image');
 const closeBtn = document.querySelector(
   'button[data-action = "close-lightbox"]',
 );
+const arrOfGalleryItems = images.map(image => createGalleryItem(image));
 
-//Add preview gallary
+galleryContainer.append(...arrOfGalleryItems);
+galleryContainer.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+lightBoxOverlay.addEventListener('click', onOverlayClick);
 
 function createGalleryItem(image) {
   const itemContainer = document.createElement('li');
@@ -22,19 +27,14 @@ function createGalleryItem(image) {
   imageItem.src = `${image.preview}`;
   imageItem.dataset.source = `${image.original}`;
   imageItem.alt = `${image.description}`;
-
-  //   imageItem.dataset.indexofimage = `${(indexOfImage += 1)}`;
   itemLink.append(imageItem);
   itemContainer.append(itemLink);
   return itemContainer;
 }
 
-const arrOfGalleryItems = images.map(image => createGalleryItem(image));
-galleryContainer.append(...arrOfGalleryItems);
-
-// Add modal window
-galleryContainer.addEventListener('click', event => {
+function openModal(event) {
   event.preventDefault();
+  window.addEventListener('keydown', onEscPress);
   const itemRef = event.target;
   if (itemRef.nodeName !== 'IMG') {
     return;
@@ -44,17 +44,23 @@ galleryContainer.addEventListener('click', event => {
   lightBox.classList.add('is-open');
   lightBoxImage.src = largImgeUrl;
   lightBoxImage.alt = itemRef.dataset.alt;
-  //   lightBoxImage.src = `${images.source}`;
-  //   lightBoxImage.alt = `${images[largImgIndex].description}`;
+}
 
-  //   addWindowListener();
-  // lightBoxImage.alt = largImgeUrl;
-});
-
-//Close modalwindov
-closeBtn.addEventListener('click', closeModal);
 function closeModal() {
+  window.removeEventListener('keydown', onEscPress);
   lightBox.classList.remove('is-open');
   lightBoxImage.src = '';
   lightBoxImage.alt = '';
+}
+
+function onOverlayClick(event) {
+  // console.log(event.target);
+  if (event.target === event.currentTarget) {
+    closeModal();
+  }
+}
+function onEscPress(event) {
+  if (event.code === 'Escape') {
+    closeModal();
+  }
 }
